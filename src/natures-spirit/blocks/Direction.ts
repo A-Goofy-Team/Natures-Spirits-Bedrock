6184ad9557014cfbe68a1e62b0a48afa5c0f6bcf
Goofy9506 <goofy9605@protonmail.com>
2025-05-14 11:01:15 -0500

    feat: :sparkles: leaves unstable | upd direction.ts

--- src/natures-spirit/blocks/Direction.ts
+++ src/natures-spirit/blocks/Direction.ts
@@ -14,51 +14,51 @@ world.beforeEvents.worldInitialize.subscribe(({ blockComponentRegistry }) => {
     }
   );
 
-  //   blockComponentRegistry.registerCustomComponent(
-  //     "natures_spirit:block_face_3",
-  //     {
-  //       beforeOnPlayerPlace(arg) {
-  //         // const face = arg.face;
+  blockComponentRegistry.registerCustomComponent(
+    "natures_spirit:block_face_3",
+    {
+      beforeOnPlayerPlace(arg) {
+        // const face = arg.face;
 
-  //         // const Faces = {
-  //         //   ["Up"]: 0,
-  //         //   ["Down"]: 0,
-  //         //   ["North"]: 1,
-  //         //   ["South"]: 1,
-  //         //   ["West"]: 2,
-  //         //   ["East"]: 2,
-  //         // };
+        // const Faces = {
+        //   ["Up"]: 0,
+        //   ["Down"]: 0,
+        //   ["North"]: 1,
+        //   ["South"]: 1,
+        //   ["West"]: 2,
+        //   ["East"]: 2,
+        // };
 
-  //         // arg.permutationToPlace = arg.permutationToPlace.withState(
-  //         //   "natures_spirit:block_face",
-  //         //   Faces[face]
-  //         // );
+        // arg.permutationToPlace = arg.permutationToPlace.withState(
+        //   "natures_spirit:block_face",
+        //   Faces[face]
+        // );
 
-  //         let face = arg.face;
-  //         switch (face) {
-  //           case "Up":
-  //           case "Down":
-  //             arg.permutationToPlace = arg.permutationToPlace.withState(
-  //               "natures_spirit:block_face",
-  //               0
-  //             );
-  //             break;
-  //           case "North":
-  //           case "South":
-  //             arg.permutationToPlace = arg.permutationToPlace.withState(
-  //               "natures_spirit:block_face",
-  //               1
-  //             );
-  //             break;
-  //           case "West":
-  //           case "East":
-  //             arg.permutationToPlace = arg.permutationToPlace.withState(
-  //               "natures_spirit:block_face",
-  //               2
-  //             );
-  //             break;
-  //         }
-  //       },
-  //     }
-  //   );
+        let face = arg.face;
+        switch (face) {
+          case "Up":
+          case "Down":
+            arg.permutationToPlace = arg.permutationToPlace.withState(
+              "natures_spirit:block_face" as any,
+              0
+            );
+            break;
+          case "North":
+          case "South":
+            arg.permutationToPlace = arg.permutationToPlace.withState(
+              "natures_spirit:block_face" as any,
+              1
+            );
+            break;
+          case "West":
+          case "East":
+            arg.permutationToPlace = arg.permutationToPlace.withState(
+              "natures_spirit:block_face" as any,
+              2
+            );
+            break;
+        }
+      },
+    }
+  );
 });
--- src/natures-spirit/blocks/Leaves.ts
+++ src/natures-spirit/blocks/Leaves.ts
@@ -1,106 +1,107 @@
-import {
-  world,
-  BlockPermutation,
-  Block,
-} from "@minecraft/server";
+///// VEry EXPIREMENTAl
+// `import {
+//   world,
+//   BlockPermutation,
+//   Block,
+// } from "@minecraft/server";
 
-const allowedBlocks = ["minecraft:oak_log", "natures_spirit:aspen_log"];
+// const allowedBlocks = ["minecraft:oak_log", "natures_spirit:aspen_log"];
 
-world.beforeEvents.worldInitialize.subscribe((eventData) => {
-  eventData.blockComponentRegistry.registerCustomComponent(
-    "natures_spirit:on_random_tick",
-    {
-      onRandomTick(e) {
-        const { block } = e;
-        const { x, y, z } = block.location;
+// world.beforeEvents.worldInitialize.subscribe((eventData) => {
+//   eventData.blockComponentRegistry.registerCustomComponent(
+//     "natures_spirit:on_random_tick",
+//     {
+//       onRandomTick(e) {
+//         const { block } = e;
+//         const { x, y, z } = block.location;
 
-        const persistent = isConnectedToAllowedBlock(block, 4);
+//         const persistent = isConnectedToAllowedBlock(block, 4);
 
-        const currentStates = block.permutation.getAllStates();
-        const newStates = {
-          ...currentStates,
-          "natures_spirit:persistent_bit": persistent,
-        };
-        const newPermutation = BlockPermutation.resolve(
-          block.typeId,
-          newStates
-        );
-        block.setPermutation(newPermutation);
+//         const currentStates = block.permutation.getAllStates();
+//         const newStates = {
+//           ...currentStates,
+//           "natures_spirit:persistent_bit": persistent,
+//         };
+//         const newPermutation = BlockPermutation.resolve(
+//           block.typeId,
+//           newStates
+//         );
+//         block.setPermutation(newPermutation);
 
-        if (
-          !persistent &&
-          !block.permutation.getState("natures_spirit:placed" as any)
-        ) {
-          block.dimension.runCommand(`/setblock ${x} ${y} ${z} air destroy`);
-        }
-      },
-    }
-  );
-});
+//         if (
+//           !persistent &&
+//           !block.permutation.getState("natures_spirit:placed" as any)
+//         ) {
+//           block.dimension.runCommand(`/setblock ${x} ${y} ${z} air destroy`);
+//         }
+//       },
+//     }
+//   );
+// });
 
-function isConnectedToAllowedBlock(
-  block: Block,
-  maxDistance: number
-): boolean {
-  const queue: Array<{ block: Block; dist: number }> = [{ block, dist: 0 }];
-  const visited = new Set<string>();
+// function isConnectedToAllowedBlock(
+//   block: Block,
+//   maxDistance: number
+// ): boolean {
+//   const queue: Array<{ block: Block; dist: number }> = [{ block, dist: 0 }];
+//   const visited = new Set<string>();
 
-  while (queue.length) {
-    const { block: currentBlock, dist } = queue.shift()!;
-    if (dist > maxDistance) continue;
+//   while (queue.length) {
+//     const { block: currentBlock, dist } = queue.shift()!;
+//     if (dist > maxDistance) continue;
 
-    const { x, y, z } = currentBlock.location;
-    const blockKey = `${x},${y},${z}`;
-    if (visited.has(blockKey)) continue;
-    visited.add(blockKey);
+//     const { x, y, z } = currentBlock.location;
+//     const blockKey = `${x},${y},${z}`;
+//     if (visited.has(blockKey)) continue;
+//     visited.add(blockKey);
 
-    if (allowedBlocks.includes(currentBlock.typeId)) return true;
+//     if (allowedBlocks.includes(currentBlock.typeId)) return true;
 
-    if (
-      currentBlock.typeId.startsWith("natures_spirit:") &&
-      currentBlock.typeId.endsWith("_leaves")
-    ) {
-      const persistentBit = currentBlock.permutation.getState(
-        "natures_spirit:persistent_bit" as any
-      );
-      if (persistentBit || dist === 0) {
-        for (const adjacentBlock of [
-          currentBlock.above(),
-          currentBlock.below(),
-          currentBlock.north(),
-          currentBlock.south(),
-          currentBlock.east(),
-          currentBlock.west(),
-        ]) {
-          if (
-            adjacentBlock &&
-            !visited.has(
-              `${adjacentBlock.location.x},${adjacentBlock.location.y},${adjacentBlock.location.z}`
-            )
-          ) {
-            queue.push({ block: adjacentBlock, dist: dist + 1 });
-          }
-        }
-      }
-    }
-  }
-  return false;
-}
+//     if (
+//       currentBlock.typeId.startsWith("natures_spirit:") &&
+//       currentBlock.typeId.endsWith("_leaves")
+//     ) {
+//       const persistentBit = currentBlock.permutation.getState(
+//         "natures_spirit:persistent_bit" as any
+//       );
+//       if (persistentBit || dist === 0) {
+//         for (const adjacentBlock of [
+//           currentBlock.above(),
+//           currentBlock.below(),
+//           currentBlock.north(),
+//           currentBlock.south(),
+//           currentBlock.east(),
+//           currentBlock.west(),
+//         ]) {
+//           if (
+//             adjacentBlock &&
+//             !visited.has(
+//               `${adjacentBlock.location.x},${adjacentBlock.location.y},${adjacentBlock.location.z}`
+//             )
+//           ) {
+//             queue.push({ block: adjacentBlock, dist: dist + 1 });
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return false;
+// }
 
-world.afterEvents.playerPlaceBlock.subscribe((eventData) => {
-  const { block } = eventData;
+// world.afterEvents.playerPlaceBlock.subscribe((eventData) => {
+//   const { block } = eventData;
 
-  if (
-    block.typeId.startsWith("natures_spirit:") &&
-    block.typeId.endsWith("_leaves") &&
-    !block.permutation.getState("natures_spirit:placed" as any)
-  ) {
-    const currentStates = block.permutation.getAllStates();
+//   if (
+//     block.typeId.startsWith("natures_spirit:") &&
+//     block.typeId.endsWith("_leaves") &&
+//     !block.permutation.getState("natures_spirit:placed" as any)
+//   ) {
+//     const currentStates = block.permutation.getAllStates();
 
-    const newStates = { ...currentStates, "natures_spirit:placed": true };
+//     const newStates = { ...currentStates, "natures_spirit:placed": true };
 
-    const newPermutation = BlockPermutation.resolve(block.typeId, newStates);
+//     const newPermutation = BlockPermutation.resolve(block.typeId, newStates);
 
-    block.setPermutation(newPermutation);
-  }
-});
+//     block.setPermutation(newPermutation);
+//   }
+// });
